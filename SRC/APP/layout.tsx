300304e789960bfe4a67cbd47dbdaa806846256f
Marcio Queiroz <marcioqueiroz@Marcio-Queiroz.local>
2025-08-25 09:33:24 -0300

    Versão inicial do projeto

---
+++ SRC/APP/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import { Toaster } from "@/components/ui/toaster"
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Scriptura Lumina',
+  description: 'Seu dicionário e comentário bíblico com IA.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" className="dark">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Source+Code+Pro:wght@400;500&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased">
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
